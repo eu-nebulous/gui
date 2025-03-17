958ef71d149fc2f6d2483ead9f8d157ee6bdf0d3
Fotis Paraskevopoulos <fotis@existanze.com>
2025-03-17 21:23:28 +0200

    Fixing RAW output slide options

--- gui/src/constants/metricsOptions.ts
+++ gui/src/constants/metricsOptions.ts
@@ -1,4 +1,5 @@
 export const behaviorOptions = ["all", "sliding"] as const
+export const behaviorRawOptions =  ["all", "first","last"]  as const
 export const behaviorOptionsInput = ["batch", "sliding"] as const
 export const behaviorOptionsOutput = ["all", "first","last"] as const
 export const unitTimeOptions = ["ms", "sec", "min", "hour", "day", "events",] as const
