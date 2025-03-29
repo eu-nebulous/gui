f59aa407b85a7bc2b27ef4c86796a97e7514c555
Fotis Paraskevopoulos <fotis@existanze.com>
2025-03-29 17:56:48 +0200

    https://openproject.nebulouscloud.eu/projects/nebulous-2nd-release-1/work_packages/702/activity

--- gui/src/components/Modal/ResourceCreation.vue
+++ gui/src/components/Modal/ResourceCreation.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="flex flex-col text-start">
     <slot name="title"></slot>
-    <Dialog.Description>
-      <ResourceForm :resource-data="resourceData" :rules="rules"/>
-    </Dialog.Description>
-    <Dialog.Footer>
-      <Button type="button" variant="outline-secondary" @click="closeModal(false)" class="w-20 mr-4"> Cancel </Button>
-      <Button variant="primary" type="button" class="w-20" @click="createResource"> Create </Button>
-    </Dialog.Footer>
+    <ResourceForm :resource-data="resourceData"
+                  :externalResults="externalResults"
+                  @on-confirm="createResource"
+                  @on-close="closeModal"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import {reactive} from "vue"
+import {reactive, ref} from "vue"
 import {useUIStore} from "@/store/modules/ui.ts"
 import {useResourceStore} from "@/store/modules/resources.ts"
 import {extractExternalResults} from "@/utils/helper.ts"
-import {useVuelidate} from "@vuelidate/core"
-import {required} from "@vuelidate/validators"
-import {Dialog} from "@/base-components/Headless"
-import Button from "@/base-components/Button"
 import {SNACKBAR_MESSAGE_TYPES} from "@/constants"
 import {IResourcePayload} from "@/types/resource.ts"
 import ResourceForm from "@/components/Modal/ResourceForm.vue";
 import {IPlatform} from "@/interfaces/platform.interface.ts";
-import { IRegion, IType } from "@/interfaces/resources.interface.ts"
 
 const resourceStore = useResourceStore()
 const uiStore = useUIStore()
@@ -46,6 +39,7 @@ const resourceData = reactive<IResourcePayload>({
     user:"",
     secret:"",
     domain:"",
+    subscriptionId:"",
   },
   sshCredentials: {
     username:"",
@@ -54,41 +48,13 @@ const resourceData = reactive<IResourcePayload>({
   }
 })
 
-// HACK: https://github.com/vuelidate/vuelidate/issues/1147
-const externalServerValidation = () => true
-
-const rules = {
-  title: { required, externalServerValidation },
-  platform: { required, externalServerValidation },
-  credentials: {
-    user: { required, externalServerValidation },
-    secret: { required, externalServerValidation },
-  }
-}
-
-const $externalResults = reactive({})
-const v$ = useVuelidate(rules, resourceData, { $externalResults })
-
 const closeModal = (skipConfirmation: boolean = false) => {
   uiStore.setModalWindowState(null, skipConfirmation)
 }
 
+const externalResults = reactive({})
 
 const createResource = async () => {
-  
-  if (!(await v$.value.$validate())) {
-    console.log("Failed validation");
-    return;
-  }
-  // Map regions from objects
-  if (Array.isArray(resourceData.regions)) {
-    resourceData.regions = resourceData.regions.map((region: IRegion) => region.region).join(',');
-  }
-  // Map validInstanceTypes from objects
-  if (Array.isArray(resourceData.validInstanceTypes)) {
-    resourceData.validInstanceTypes = resourceData.validInstanceTypes.map((type: IType) => type.instanceType).join(',');
-  }
-
   resourceStore
       .createResource(resourceData)
       .then((createdResource) => {
@@ -100,7 +66,7 @@ const createResource = async () => {
       })
       .catch((error) => {
         const errors = extractExternalResults(error);
-        Object.assign($externalResults, errors);
+        Object.assign(externalResults, errors);
       });
 };
 
